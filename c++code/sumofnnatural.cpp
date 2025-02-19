9b132e35d123d8777a2cc87c72780f30c2d4f79e
HARSHIT KUMAR <harshitkr9696@gmail.com>
2025-02-19 10:07:35 +0530

    adding code of sum of n natural number in cpp.

---
+++ c++code/sumofnnatural.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define SUMN(X) (X*(X+1)/2)
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"\n\n\n\t\t\tEnter the value of n : ";
+    cin>>n;
+    cout<<"\n\n\n\t\t\tThe value of sum of n natural number is : "<<SUMN(n);
+    return 0;
+}
