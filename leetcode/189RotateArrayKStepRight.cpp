953a0d2328986f677e0534286aca2ca9df17d4ce
HARSHIT KUMAR <harshitkr9696@gmail.com>
2025-02-26 13:01:08 +0530

    Added the rotate array k step code in leetcode folder

--- leetcode/189RotateArrayKStepRight.cpp
+++ leetcode/189RotateArrayKStepRight.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 
 using namespace std;
+
 void rotateRight(vector<int> &nums,int k){
     // checking the codition for step greater than arrays length.
     k%=nums.size();
@@ -26,9 +27,22 @@ void display(vector<int> &nums){
         cout<<"\t"<<it;
     }
 }
+
+vector<int> rotateRight1(vector<int> &nums,int k){
+    int n=nums.size();
+    k%=n;
+    vector<int> temp(n);
+    for(int i=0;i<n;i++){
+        temp[i]=nums[(i+k-1)%n];
+    }
+    return temp;
+}
+
 int main(){
     vector<int> nums{1,2,3,4,5,6,7};
     display(nums);
+    vector<int> n=rotateRight1(nums,3);
+    display(n);
     rotateRight(nums,3);
     display(nums);
     return 0;
